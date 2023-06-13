74a45d9c2234664c25360d76fa6a9c975ac9e538
Paul Loffreda <ploffreda@gspp.com>
2023-06-13 16:07:22 -0400

    FrontEnd start

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,14 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
+
 
 // @desc      Get Goals
 // @route     GET /api/goals
 // @access    Private
 const getGoals = asyncHandler(async (req,res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
     res.status(200).json(goals)
 })
 
@@ -20,7 +22,8 @@ const setGoal = asyncHandler(async (req,res) => {
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
     res.status(200).json(goal)
 })
@@ -34,6 +37,18 @@ const updateGoal = asyncHandler(async (req,res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    // Check for user
+    if(!req.user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+    // Make sure the login user matches the goal user
+    if(goal.user.toString() !== req.user.id ){
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
     })
@@ -50,8 +65,20 @@ const deleteGoal = asyncHandler(async (req,res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    // Check for user
+    if(!req.user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+    // Make sure the login user matches the goal user
+    if (goal.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
     
-    await goal.remove()
+    //mongoose no longer supports .remove()
+    await goal.deleteOne()
 
     res.status(200).json({id: req.params.id})
 })
@@ -60,5 +87,5 @@ module.exports = {
     getGoals,
     setGoal,
     updateGoal,
-    deleteGoal
+    deleteGoal,
 }
