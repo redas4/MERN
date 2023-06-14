9602f806ca725f99d0e3ad6a7ff9b40c02dee200
Paul Loffreda <ploffreda@gspp.com>
2023-06-14 15:41:23 -0400

    Addded Frontend Authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 
 import {BrowserRouter as Router, Routes, Route} from 
 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header'
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -9,6 +12,7 @@ function App() {
     <> 
     <Router>
     <div className='container'>
+      <Header />
       <Routes>
         <Route path='/' element={<Dashboard />} />
         <Route path='/login' element={<Login />} />
@@ -17,6 +21,7 @@ function App() {
       </Routes>
     </div>
     </Router>
+    <ToastContainer />
     </>
   );
 }
---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+const API_URL = '/api/users/'
+
+//Register user
+const register = async (userData) => {
+    const response = await axios.post(API_URL, userData)
+
+    if(response.Data){
+        localStorage.setItem('user', JSON.stringify)
+    }
+
+    return response.data
+}
+
+//Register user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if(response.Data){
+        localStorage.setItem('user', JSON.stringify)
+    }
+
+    return response.data
+}
+
+//Logout user
+const logout = () => {
+    localStorage.removeItem('user')
+}
+
+const authService = {
+    register,
+    logout,
+    login,
+}
+
+export default authService
+
